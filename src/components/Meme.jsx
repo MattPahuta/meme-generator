200d6201bd14b65de826d9943849a07128ae3809
Matt Pahuta <mhpahuta@gmail.com>
2024-01-12 15:52:42 -0800

    Add Meme component with basic styles

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,11 +1,21 @@
 export default function Meme() {
+
+
+
+  
   return (
     <main>
-      <form className="form">
-        <input type="text" placeholder="Top text" className="form-input" />
-        <input type="text" placeholder="Bottom text" className="form-input" />
+      <div className="form">
+        <div>
+          <label htmlFor="top-text"></label>
+          <input type="text" placeholder="Shut up" id="top-text" className="form-input" />
+        </div>    
+        <div>
+          <label htmlFor="bottom-text"></label>
+          <input type="text" placeholder="add take my money" id="bottom-text" className="form-input" />
+        </div>
         <button className="form-button">Get a new meme image 🖼️</button>
-      </form>
+      </div>
     </main>
   )
 }
