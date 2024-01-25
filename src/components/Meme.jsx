8ef002c743b96912e9f4ab17945164e3d7d2096d
Matt Pahuta <mhpahuta@gmail.com>
2024-01-25 14:31:47 -0800

    Update Meme component with random meme generating logic

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,8 +1,30 @@
+import memesData from "../memesData"
+
 export default function Meme() {
 
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg"
+  })
 
+  const [allMemeImages, setAllMemeImages] = React.useState(memesData)
 
-  
+  function getMemeImage() {
+
+    const memesArray = allMemeImages.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    
+    setMeme(prevMeme => {
+      return {
+        ...prevMeme,
+        randomImage: url
+      }
+    })
+
+  }
+
   return (
     <main>
       <div className="form">
@@ -14,8 +36,9 @@ export default function Meme() {
           <label htmlFor="bottom-text"></label>
           <input type="text" placeholder="add take my money" id="bottom-text" className="form-input" />
         </div>
-        <button className="form-button">Get a new meme image 🖼️</button>
+        <button onClick={getMemeImage} className="form-button">Get a new meme image 🖼️</button>
       </div>
+      <img src={meme.randomImage} className="meme--image" alt="" />
     </main>
   )
 }
