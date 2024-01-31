1b2dd29a3cfe0427de7f14284b57ec93ad2029cf
Matt Pahuta <mhpahuta@gmail.com>
2024-01-31 15:07:53 -0800

    Add useEffect hook for API call of memes

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,4 +1,6 @@
-import memesData from "../memesData"
+// import memesData from "../memesData"
+
+import { useEffect } from "react";
 
 export default function Meme() {
 
@@ -8,12 +10,19 @@ export default function Meme() {
     randomImage: "http://i.imgflip.com/1bij.jpg"
   })
 
-  const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+  const [allMemes, setAllMemes] = React.useState([])
+
+  // useEffect hook for API call
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then(res => res.json())
+      .then(data => setAllMemes(data.data.memes))
+  }, [])
 
   function getMemeImage() {
-    const memesArray = allMemeImages.data.memes;
-    const randomNumber = Math.floor(Math.random() * memesArray.length);
-    const url = memesArray[randomNumber].url;
+    // const memesArray = allMemes.data.memes;
+    const randomNumber = Math.floor(Math.random() * allMemes.length);
+    const url = allMemes[randomNumber].url;
     setMeme(prevMeme => {
       return {
         ...prevMeme,
